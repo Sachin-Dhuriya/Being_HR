5a0a40522e1dfbbd5e41f5bce9c9e9eb726d8b52
Sachin-Dhuriya <sachindhuriya561@gmail.com>
2025-01-22 14:54:32 +0530

    Implimented Cup and medal emojis for Top 3 in each category

--- frontend/src/Components/Cards/NomineeRow.jsx
+++ frontend/src/Components/Cards/NomineeRow.jsx
@@ -1,11 +1,22 @@
-// NomineeRow.js
 import React from 'react';
 import './NomineeRow.css';
 
 const NomineeRow = ({ rank, fullName, title, category, company, votes, juryVotes, totalScore }) => {
+  // Determine the emoji based on rank
+  const getRankEmoji = (rank) => {
+    if (rank === 1) {
+      return '🏆 #1';  // Winner cup for rank 1
+    } else if (rank === 2) {
+      return '🥈 #2';  // Silver medal for rank 2
+    } else if (rank === 3) {
+      return '🥉 #3';  // Bronze medal for rank 3
+    }
+    return `#${rank}`;  // Default format for other ranks
+  };
+
   return (
     <tr className="nominee-row">
-      <td className="rank">{rank}</td>
+      <td className="rank">{getRankEmoji(rank)}</td>
       <td>
         <div className="nominee-info">
           <span className="nominee-name">{fullName}</span>
@@ -14,11 +25,9 @@ const NomineeRow = ({ rank, fullName, title, category, company, votes, juryVotes
       </td>
       <td>{category}</td>
       <td>{company}</td>
-      
-      <td > <div id='num'>{votes}</div></td>
-      <td ><div id='juryvotes'> {juryVotes || 0}</div></td>
-      <td ><div id='totalscore'> {votes + (juryVotes || 0)}</div></td>
-     
+      <td><div id='num'>{votes}</div></td>
+      <td><div id='juryvotes'>{juryVotes || 0}</div></td>
+      <td><div id='totalscore'>{votes + (juryVotes || 0)}</div></td>
     </tr>
   );
 };
